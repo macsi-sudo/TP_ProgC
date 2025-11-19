0f1164f86b8faaa1920072749e3a1c7da2a1a156
macsi-sudo <macsissoko10@gmail.com>
2025-11-19 14:27:50 +0100

    Update fibonacci.c

---
+++ TP2/src/fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+    int n;
+
+    printf("Entrez la valeur de n : ");
+    scanf("%d", &n);
+
+    int a = 0, b = 1, c;
+
+    printf("%d ", a);
+    if (n >= 1) printf("%d ", b);
+
+    for (int i = 2; i <= n; i++) {
+        c = a + b;
+        printf("%d ", c);
+        a = b;
+        b = c;
+    }
+
+    printf("\n");
+    return 0;
+}
