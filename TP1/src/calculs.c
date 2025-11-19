39bcfe8b2581807dce323d9c5e94ea3f3dc3dafe
macsi-sudo <macsissoko10@gmail.com>
2025-11-19 13:49:51 +0100

    Update calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,54 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+    char op;
+
+    printf("Entrez num1 : ");
+    scanf("%d", &num1);
+
+    printf("Entrez num2 : ");
+    scanf("%d", &num2);
+
+    printf("Entrez l'operateur (+ - * / %% & | ~) : ");
+    scanf(" %c", &op);
+
+    switch(op) {
+        case '+':
+            printf("Résultat : %d\n", num1 + num2);
+            break;
+        case '-':
+            printf("Résultat : %d\n", num1 - num2);
+            break;
+        case '*':
+            printf("Résultat : %d\n", num1 * num2);
+            break;
+        case '/':
+            if (num2 != 0)
+                printf("Résultat : %d\n", num1 / num2);
+            else
+                printf("Erreur : division par zéro\n");
+            break;
+        case '%':
+            if (num2 != 0)
+                printf("Résultat : %d\n", num1 % num2);
+            else
+                printf("Erreur : modulo par zéro\n");
+            break;
+        case '&':
+            printf("Résultat : %d\n", num1 & num2);
+            break;
+        case '|':
+            printf("Résultat : %d\n", num1 | num2);
+            break;
+        case '~':
+            printf("Résultat : ~num1 = %d, ~num2 = %d\n", ~num1, ~num2);
+            break;
+        default:
+            printf("Opérateur invalide !\n");
+    }
+
+    return 0;
+}
+
 
